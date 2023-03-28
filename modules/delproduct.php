27a45cd903c31e840593601e2aba36432d073a85
xf0r3m <jakubstasinski@protonmail.com>
2023-03-28 20:09:32 +0200

    modules/delproduct.php - Usunięcie produktu, powoduje usunię powiązanych z nim komponentów

--- modules/delproduct.php
+++ modules/delproduct.php
@@ -1,5 +1,14 @@
 <?php
   $id = intval($_POST["delPid"]);
+  $tableName = 'component';
+  $whereValue = "productId = " . $id;
+  $result = dbDel($connection, $tableName, $whereValue);
+  if ( $result == true ) {
+    echo "<div class=\"alert alert-success\" role=\"alert\">Usunięto wszystkie komponenty produktu</div>";
+  } else {
+    echo "<div class=\"alert alert-danger\" role=\"alert\">Komponenty produktu nie zostały usunięte</div>";
+  }
+
   $tableName = 'product';
   $whereValue = "id = " . $id;
   $result = dbDel($connection, $tableName, $whereValue);
