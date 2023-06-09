7b6a321a4c2e9090c2b01e8ccd57e51d0cb42bb7
xf0r3m <jakubstasinski@protonmail.com>
2023-06-09 17:31:55 +0200

    Lista ogólnodostępna sortowanie - BT #51 - optymalizacja

--- bugs.php
+++ bugs.php
@@ -4,43 +4,8 @@
   </div>
   <div class="card-body">
 <?php
-  $tableName = 'bug';
-  $columnScheme = "*";
-  $whereValue = "state > 0 AND state < 3 ORDER BY id DESC";
-  $result = dbQuery($connection, $tableName, $columnScheme, $whereValue);
-  if ( mysqli_num_rows($result) > 0 ) {
-    echo "<table class=\"table\">";
-    echo "<thead><tr><th scope=\"col\">#Lp.</th><th scope=\"col\">#id</th><th scope=\"col\">Produkt</th><th scope=\"col\">Komponent</th>";
-    echo "<th scope=\"col\">Rodzaj zgłosz.</th><th scope=\"col\">Temat zgłosz.</th><th scope=\"col\">Opis zgłosz.</th>";
-    echo "<th>Status zgłosz.</th></tr></thead>";
-    echo "<tbody>";
-    $n = 1;
-    while ( $row = mysqli_fetch_row($result) ) {
-      echo "<tr><td>" . $n . "</td><td><a href=\"?p=comments&bid=" . $row[0] . "\">#" . $row[0] . "</a></td><th scope=\"row\">";
-      $tableName = 'product';
-      $columnScheme = 'name';
-      $whereValue = 'id = ' . intval($row[1]);
-      $result2 = dbQuery($connection, $tableName, $columnScheme, $whereValue);
-      echo getFieldValue($result2);
-      echo "</th><th>";
-      $tableName = 'component';
-      $columnScheme = 'name';
-      $whereValue = 'id = ' . intval($row[2]);
-      $result3 = dbQuery($connection, $tableName, $columnScheme, $whereValue);
-      echo getFieldValue($result3);
-      echo "</th><td>" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td>";
-      echo "<td>";
-      $stateTbl = array("Przyjęty", "Potwierdzony", "W trakcie", "Zakończony", "Odrzucony");
-      $index = $row[6];
-      echo $stateTbl[$index]; 
-      echo "</td>";
-      ++$n;
-    }
-    echo "</tbody>";
-    echo "</table>";
-  } else {
-    echo "<div class=\"alert alert-primary\" role=\"alert\">Nie znaleziono żadnych zgłoszeń w trakcie realizacji</div>";
-  }
+  $cond = "state > 0 AND state < 3 ORDER BY id DESC";
+  presentListBugs($connection, $cond);
 ?>
   </div>
 </div>
@@ -51,43 +16,8 @@
   </div>
   <div class="card-body">
 <?php
-  $tableName = 'bug';
-  $columnScheme = "*";
-  $whereValue = "state >= 3 ORDER BY id DESC";
-  $result = dbQuery($connection, $tableName, $columnScheme, $whereValue);
-  if ( mysqli_num_rows($result) > 0 ) {
-    echo "<table class=\"table\">";
-    echo "<thead><tr><th scope=\"col\">#Lp.</th><th scope=\"col\">#id</th><th scope=\"col\">Produkt</th><th scope=\"col\">Komponent</th>";
-    echo "<th scope=\"col\">Rodzaj zgłosz.</th><th scope=\"col\">Temat zgłosz.</th><th scope=\"col\">Opis zgłosz.</th>";
-    echo "<th>Status zgłosz.</th></tr></thead>";
-    echo "<tbody>";
-    $n = 1;
-    while ( $row = mysqli_fetch_row($result) ) {
-      echo "<tr><td>" . $n . "</td><td><a href=\"?p=comments&bid=" . $row[0] . "\">#" . $row[0] . "</a></td><th scope=\"row\">";
-      $tableName = 'product';
-      $columnScheme = 'name';
-      $whereValue = 'id = ' . intval($row[1]);
-      $result2 = dbQuery($connection, $tableName, $columnScheme, $whereValue);
-      echo getFieldValue($result2);
-      echo "</th><th>";
-      $tableName = 'component';
-      $columnScheme = 'name';
-      $whereValue = 'id = ' . intval($row[2]);
-      $result3 = dbQuery($connection, $tableName, $columnScheme, $whereValue);
-      echo getFieldValue($result3);
-      echo "</th><td>" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td>";
-      echo "<td>";
-      $stateTbl = array("Przyjęty", "Potwierdzony", "W trakcie", "Zakończony", "Odrzucony");
-      $index = $row[6];
-      echo $stateTbl[$index]; 
-      echo "</td>";
-      ++$n;
-    }
-    echo "</tbody>";
-    echo "</table>";
-  } else {
-    echo "<div class=\"alert alert-primary\" role=\"alert\">Nie znaleziono żadnych zamkniętych zgłoszeń</div>";
-  }
+  $cond = "state >= 3 ORDER BY id DESC";
+  presentListBugs($connection, $cond);
 ?>
   </div>
 </div>
--- library.php
+++ library.php
@@ -103,5 +103,43 @@ function newFormatTo80Cols($long_string, $linePrefix, $eolSign) {
   return $content;
 }
 
+function presentListBugs($connection, $cond) {
+  $tableName = 'bug';
+  $columnScheme = "*";
+  $whereValue = $cond;
+  $result = dbQuery($connection, $tableName, $columnScheme, $whereValue);
+  if ( mysqli_num_rows($result) > 0 ) {
+    echo "<table class=\"table\">";
+    echo "<thead><tr><th scope=\"col\">#Lp.</th><th scope=\"col\">#id</th><th scope=\"col\">Produkt</th><th scope=\"col\">Komponent</th>";
+    echo "<th scope=\"col\">Rodzaj zgłosz.</th><th scope=\"col\">Temat zgłosz.</th><th scope=\"col\">Opis zgłosz.</th>";
+    echo "<th>Status zgłosz.</th></tr></thead>";
+    echo "<tbody>";
+    $n = 1;
+    while ( $row = mysqli_fetch_row($result) ) {
+      echo "<tr><td>" . $n . "</td><td><a href=\"?p=comments&bid=" . $row[0] . "\">#" . $row[0] . "</a></td><th scope=\"row\">";
+      $tableName = 'product';
+      $columnScheme = 'name';
+      $whereValue = 'id = ' . intval($row[1]);
+      $result2 = dbQuery($connection, $tableName, $columnScheme, $whereValue);
+      echo getFieldValue($result2);
+      echo "</th><th>";
+      $tableName = 'component';
+      $columnScheme = 'name';
+      $whereValue = 'id = ' . intval($row[2]);
+      $result3 = dbQuery($connection, $tableName, $columnScheme, $whereValue);
+      echo getFieldValue($result3);
+      echo "</th><td>" . $row[3] . "</td><td>" . $row[4] . "</td><td>" . $row[5] . "</td>";
+      echo "<td>";
+      $stateTbl = array("Przyjęty", "Potwierdzony", "W trakcie", "Zakończony", "Odrzucony");
+      $index = $row[6];
+      echo $stateTbl[$index]; 
+      echo "</td>";
+      ++$n;
+    }
+    echo "</tbody>";
+    echo "</table>";
+  } else {
+    echo "<div class=\"alert alert-primary\" role=\"alert\">Nie znaleziono żadnych zgłoszeń w trakcie realizacji</div>";
+  }
+}
 ?>
-          
